30c02c49b1369958e1a5d8ed9d64bf074e13da51
theme_wm@163.com <YLng1996>
2020-04-26 16:19:07 +0800

    update sql

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,9 @@ killTomcat()
       kill -9 $pid
     fi
 }
-cd $PROJ_PATH/order
+
+cd $PROJ_PATH
+
 mvn clean install
 
 # 停tomcat
@@ -32,7 +34,7 @@ rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/target/order.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
 mv order.war ROOT.war
